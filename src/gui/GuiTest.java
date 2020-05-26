aa158067c433956639f4a6e8731b69e690ae564c
saleese <saleese@gmail.com>
2020-05-26 10:45:10 +0900

    added GUIs

---
+++ src/gui/GuiTest.java
@@ -0,0 +1,11 @@
+package gui;
+
+public class GuiTest {
+
+	public static void main(String[] args) {
+		//MenuSelection menuselection = new MenuSelection();
+		//StudentAdder studentadder = new StudentAdder();
+		StudentViewer studentviewer = new StudentViewer();
+	}
+
+}
